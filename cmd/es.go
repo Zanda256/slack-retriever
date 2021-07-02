1c4dee8956ec145643e1df1b36e38a5b63cce181
Sekiranda Hamza <sekirandahamza@gmail.com>
2021-07-02 04:01:06 +0300

    Changed datastructs.go and es.go

--- cmd/es.go
+++ cmd/es.go
@@ -9,6 +9,8 @@ import (
 func newESClient() (*elasticsearch.Client, error) {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
-		log.Fatalf("Error creating the client: %s", err)
+		log.Printf("Error creating the client: %s", err)
+		return nil, err
 	}
+	return es, nil
 }
