5a5fc8fcd939d37fdb22c881cf7d72a39ca2abb4
Sekiranda Hamza <sekirandahamza@gmail.com>
2021-07-27 07:46:08 +0300

    Added storage methods

--- slack/es.go
+++ slack/es.go
@@ -1,18 +1,14 @@
 package slack
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
-
-// "service_name": "Leg end nary a laugh, Ink.",
-// "text": "This is likely a pun about the weather.",
-// "fallback": "We're withholding a pun from you",
-// "thumb_url": "https://badpuns.example.com/puns/123.png",
-// "thumb_width": 1920,
-// "thumb_height": 700,
-// "id": 1
 
 var RawMapping = `{"mappings":
 					{"dynamic":true, 
@@ -20,18 +16,18 @@ var RawMapping = `{"mappings":
 						{   "ok":{"type":"bool"}, 
 							"messages": {"type": "nested", 
 								"properties" : {
-									"type":{"type":"keyword"}
-									"user":{"type":"keyword"}
-									"text":{"type":"text"}
-									"ts":{"type":"float"}
+									"type":{"type":"keyword"},
+									"user":{"type":"keyword"},
+									"text":{"type":"text"},
+									"ts":{"type":"float"},
 									"attachments":{"type":"nested",
 										"properties": {
-											"service_name":{"type":"text"}
-											"text": {"type":"text"}
-											"fallback": {"type":"text"}
-											"thumb_url": {"type":"text"}
-											"thumb_width": {"type":"integer"}
-											"thumb_height": {"type":"integer"}
+											"service_name":{"type":"text"},
+											"text": {"type":"text"},
+											"fallback": {"type":"text"},
+											"thumb_url": {"type":"text"},
+											"thumb_width": {"type":"integer"},
+											"thumb_height": {"type":"integer"},
 											"id": {"type":"integer"}
 										}
 									}
@@ -41,12 +37,39 @@ var RawMapping = `{"mappings":
 					}
 				}`
 
-//NewESClient ctreates anew elasticsearch client
-func NewESClient() (*elasticsearch.Client, error) {
+// EsStorage has es client as a field
+type EsStorage struct {
+	c *elasticsearch.Client
+}
+
+//NewEsStorage ctreates anew elasticsearch client
+func NewEsStorage() (*EsStorage, error) {
 	es, err := elasticsearch.NewDefaultClient()
 	if err != nil {
 		log.Printf("Error creating the client: %s", err)
 		return nil, err
 	}
-	return es, nil
+	s := &EsStorage{es}
+	return s, nil
+}
+
+//NewESIndex creates a new Index called "name" with mappings as "mapping"
+func (s *EsStorage) NewESIndex(name, mapping string) (bool, error) {
+	b := strings.NewReader(mapping)
+	r := &esapi.IndicesCreateRequest{
+		Index: name,
+		Body:  b,
+	}
+	ctx := context.Background()
+	resp, err := r.Do(ctx, s.c)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	if resp.IsError() {
+		err = fmt.Errorf("Failed to create index: %d %s", resp.StatusCode, name)
+		return false, err
+	}
+	fmt.Println(resp.String())
+	return true, nil
 }
